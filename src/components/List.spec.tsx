ffd1bb2a6a6d33647fad502d430a614081743e33
Maick Souza <maick_a_s@msn.com>
2022-10-07 17:21:58 -0300

    test: test list items #2

--- src/components/List.spec.tsx
+++ src/components/List.spec.tsx
@@ -4,12 +4,18 @@ import { List } from "./List";
 
 describe("List component test", () => {
   it("should render list items", () => {
-    const { getByText } = render(
+    const { getByText, queryByText, unmount } = render(
       <List initialItems={["Maick", "Neuza", "Enilda"]} />
     );
 
     expect(getByText("Enilda")).toBeDefined();
     expect(getByText("Neuza")).toBeDefined();
     expect(getByText("Maick")).toBeDefined();
+
+    unmount();
+    render(<List initialItems={["Lourival"]} />);
+
+    expect(getByText("Lourival")).toBeDefined();
+    expect(queryByText("Maick")).toBeNull();
   });
 });
